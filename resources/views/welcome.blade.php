63c00955fbe53be2562211bc971f4d3e35e41862
robertmcbathin <mercile55@yandex.ru>
2017-01-30 15:00:04 +0300

    Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('description')
+ООО "Единая транспортная карта" - оператор безналичной оплаты проезда в общественном транспорте города Чебоксары 
+@endsection
+
+@section('title')
+Единая транспортная карта
+@endsection
+
+@section('content')
+@endsection
