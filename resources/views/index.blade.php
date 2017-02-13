ba4cdb241607bd1119dd8279697a07995c42c78a
robertmcbathin <mercile55@yandex.ru>
2017-02-13 11:34:45 +0300

    index cards: DONE!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,74 +60,47 @@
         <h3 class="category__title">{{ $card_type->name }}</h2>
       </div>
       @foreach ($cards as $card)
-        @if ($card->type == $card_type->id)
+        @if (($card->type == $card_type->id) && ($card->primary == true))
             <div class="row">
-              <div class="col-md-5">
-                <img src="{{ $card->image }}" alt="{{ $card->name }}" class="rounded mx-auto d-block">
+              <div class="col-md-5 media">
+                <img src="{{ $card->image }}" alt="{{ $card->name }}" class="rounded mx-auto d-block d-flex align-self-center mr-3">
               </div>
               <div class="col-md-7 index-card">
                 <h3>{{ $card->name }}</h3>
-                <hr>
-                <blockquote> {{ $card->description }}</blockquote>
+                <blockquote> {{ $card->description }} <br><a href="#">Подробнее о карте...</a></blockquote>
               </div>
             </div>
+            <hr>
+              <div class="col-md-4 index-card-column text-center">
+                <strong>Стоимость карты</strong>
+                <p>{{ $card->price }}</p>
+              </div>
+              <div class="col-md-4 index-card-column text-center">
+                <strong>Срок службы</strong>
+                <p>{{ $card->lifetime }}</p>
+              </div>
+              <div class="col-md-4 index-card-column text-center">
+                <strong>Тариф</strong>
+                <p>{{ $card->tariff }}</p>
+              </div>
+        @elseif (($card->type == $card_type->id) && ($card->primary != true))
+          <div class="card" style="width: 220px;">
+            <img class="card-img-top" src="{{$card->image}}" alt="{{$card->name}}" height="140px">
+            <div class="card-block">
+              <h4 class="card-title">{{$card->name}}</h4>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><strong>Стоимость</strong> <span>{{$card->price}}</span></li>
+              <li class="list-group-item"><strong>Срок службы</strong> <span>{{$card->lifetime}}</span></li>
+              <li class="list-group-item"><strong>Тариф</strong> <span>{{$card->tariff}}</span></li>
+            </ul>
+            <div class="card-block">
+              <a href="#">Подробнее...</a>
+            </div>
+          </div>
         @endif
       @endforeach
     @endforeach
-    <div class="card-deck">
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-    <div class="card-deck">
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-    <div class="card-deck">
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card e-card">
-        <img class="card-img-top" src="http://placehold.it/465x300" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">Card title</h4>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
   </div> 
 </div><!-- End of card block -->
   <div class="container">
