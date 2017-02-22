6ac72ea5127bf5013a69fe0caa792557dcb869fa
robertmcbathin <mercile55@yandex.ru>
2017-02-22 21:26:36 +0300

    rebuild login page

--- resources/views/sudo/login.blade.php
+++ resources/views/sudo/login.blade.php
@@ -5,50 +5,96 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<style>body{padding-top: 60px;}</style>
-	
-    <link href="/admin/login/css/bootstrap.min.css" rel="stylesheet" />
- 
-	<link href="/admin/login/css/login-register.css" rel="stylesheet" />
-	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+        <link href="/admin/css/bootstrap.min.css" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="/admin/css/material-kit.css" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="/admin/css/demo.css" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
     <link rel="stylesheet" href="/admin/login/css/app.css">
 	
-	<script src="/admin/login/js/jquery-1.10.2.js" type="text/javascript"></script>
-	<script src="/admin/login/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="/admin/login/js/login-register.js" type="text/javascript"></script>
 
 </head>
 <body>
-    <div class="container-fluid" id="gradient">
-              
-		 <div class="modal fade login" id="loginModal">
-		      <div class="modal-dialog login animated">
-    		      <div class="modal-content">
-                    <div class="modal-body">  
-                        <div class="box">
-                             <div class="content">
-                                <div class="division">
-                                      <span>ПАНЕЛЬ УПРАВЛЕНИЯ</span>
+        <div class="page-header header-filter" id="gradient">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                    <div class="card card-signup">
+                        <form class="form" method="POST" action="{{ route('sudo.login.post') }}">
+                            <div class="header header-primary text-center">
+                                <h4 class="card-title">Вход в панель управления</h4>
+                            </div>
+                            <div class="content">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="material-icons">face</i>
+                                    </span>
+                                    <input type="text" name="username" class="form-control" placeholder="Логин">
                                 </div>
-                                <div class="error"></div>
-                                <div class="form loginBox">
-                                    <form method="POST" action="{{ route('sudo.login.post') }}" accept-charset="UTF-8">
-                                    <input id="username" class="form-control" type="text" placeholder="Логин" name="username">
-                                    <input id="password" class="form-control" type="password" placeholder="Пароль" name="password">
-                                    <input class="btn btn-default btn-login" type="submit" value="Войти"">
-                                    {{csrf_field()}}
-                                    </form>
+
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="material-icons">lock_outline</i>
+                                    </span>
+                                    <input type="password" name="password" placeholder="Пароль" class="form-control" />
                                 </div>
-                             </div>
-                        </div>
-                    </div>      
-    		      </div>
-		      </div>
-		  </div>
+
+                                <!-- If you want to add a checkbox to this form, uncomment this code
+
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="optionsCheckboxes" checked>
+                                        Subscribe to newsletter
+                                    </label>
+                                </div> -->
+                                {{csrf_field()}}
+                            </div>
+                            <div class="footer text-center">
+                            <input type="submit" class="btn btn-primary btn-simple btn-wd btn-lg" value="Войти" >
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    <script>
-        $('document').ready(function(){
-            openLoginModal();
-        });
-    </script>
 </body>
+<script src="/admin/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="/admin/js/jquery-ui.min.js" type="text/javascript"></script>
+<script src="/admin/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/admin/js/material.min.js" type="text/javascript"></script>
+<script src="/admin/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+<!-- Forms Validations Plugin -->
+<script src="/admin/js/jquery.validate.min.js"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+<script src="/admin/js/moment.min.js"></script>
+<!--  Charts Plugin -->
+<script src="/admin/js/chartist.min.js"></script>
+<!--  Plugin for the Wizard -->
+<script src="/admin/js/jquery.bootstrap-wizard.js"></script>
+<!--  Notifications Plugin    -->
+<script src="/admin/js/bootstrap-notify.js"></script>
+<!-- Vector Map plugin -->
+<script src="/admin/js/jquery-jvectormap.js"></script>
+<!-- Sliders Plugin -->
+<script src="/admin/js/nouislider.min.js"></script>
+<!--  Google Maps Plugin    -->
+<script src="/admin/js/jquery.select-bootstrap.js"></script>
+<!--  DataTables.net Plugin    -->
+<script src="/admin/js/jquery.datatables.js"></script>
+<!-- Sweet Alert 2 plugin -->
+<script src="/admin/js/sweetalert2.js"></script>
+<!--    Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="/admin/js/jasny-bootstrap.min.js"></script>
+<!--  Full Calendar Plugin    -->
+<script src="/admin/js/fullcalendar.min.js"></script>
+<!-- TagsInput Plugin -->
+<script src="/admin/js/jquery.tagsinput.js"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="/admin/js/material-dashboard.js"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
 </html>
